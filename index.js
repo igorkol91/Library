ed505dffbd7f58e905850958c6348c08c156d83a
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-07-01 15:11:56 +0200

    Add button logic for deleting and updateing the book status

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // const booksContainer = document.querySelector('.books-container');
-window.onload = () => {showBooks()};
+window.onload = () => {
+  showBooks();
+};
 const booksList = document.querySelector(".books-list");
 const createNew = document.querySelector("#add-book-btn");
 let myLibrary = [];
-const newBookForm = document.querySelector('#form-input')
+const newBookForm = document.querySelector("#form-input");
 let bookDeleteBtns = document.querySelectorAll(".book-control-btn-detele");
 let bookBtns = document.querySelectorAll(".book-control-btn");
 let booksId = 0;
 
-function Book(title, author, pages, status=false) {
-  booksId++
+function Book(title, author, pages, status = false) {
+  booksId++;
   this.id = booksId;
   this.title = title;
   this.author = author;
@@ -17,40 +19,48 @@ function Book(title, author, pages, status=false) {
   this.status = status;
 }
 
+Book.prototype.toggleBookStatus = (id) => {
+  const index = myLibrary.findIndex((b) => b.id == id);
+  const status = myLibrary[index].status;
+  myLibrary[index].status = status ? false : true;
+  showBooks();
+};
+
 function addBookToLibrary(title, author, pages) {
-  const book = new Book(title, author, pages)
+  const book = new Book(title, author, pages);
   myLibrary.push(book);
-  showUpdateBook(book);
+  showBooks();
 }
 
 function handleDeleteBook(id) {
-  //  const bookId = id
-  // let newBooks = myLibrary.filter(book => book.id !== bookId)
-   console.log(id);
-  // myLibrary = newBooks;
-  // showBooks();
-  // deleteLogic();
-  // console.log(btn);
+  const newBooks = myLibrary.filter((book) => book.id != id);
+  myLibrary = newBooks;
+  showBooks();
 }
 
-newBookForm.addEventListener('submit',(e) => {
+function toggleStatus(id) {
+  const book = new Book();
+  book.toggleBookStatus(id);
+}
+
+newBookForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let title = e.target.title.value;
   let author = e.target.author.value;
   let pages = e.target.pages.value;
   addBookToLibrary(title, author, pages);
-  e.target.title.value = '';
-  e.target.author.value = '';
-  e.target.pages.value = '';
-}
-);
+  e.target.title.value = "";
+  e.target.author.value = "";
+  e.target.pages.value = "";
+});
 
 createNew.addEventListener("click", () => {
   const form = document.querySelector("#form-input");
   form.classList.toggle("d-none");
 });
 
-function showBooks(){
+function showBooks() {
+  booksList.innerHTML = "";
   myLibrary.map((book) => {
     const trElement = document.createElement("tr");
     trElement.innerHTML = `<td>${book.title}</td>
@@ -58,31 +68,10 @@ function showBooks(){
     <td>${book.pages}</td>
     <td>${
       book.status
-      ? `<button id='btn${book.id}' data-index=${book.id} class='btn btn-warning book-control-btn'>Read</button>`
-      : `<button id='btn${book.id}' data-index=${book.id} class='btn btn-primary book-control-btn'>Read Book</button>`
+        ? `<button id=${book.id} data-index=${book.id} class='btn btn-warning book-control-btn' onclick="toggleStatus(this.id)">Read</button>`
+        : `<button id=${book.id} data-index=${book.id} class='btn btn-primary book-control-btn' onclick="toggleStatus(this.id)">Read Book</button>`
     }</td>
-    <td>${`<a id='${book.id}' data-index=${book.id} href='#' class='btn btn-danger book-control-btn-detele' onclick="handleDeleteBook(this.id)">Remove</a>`}</td>`;
+    <td>${`<a id=${book.id} data-index=${book.id} href='#' class='btn btn-danger book-control-btn-detele' onclick="handleDeleteBook(this.id)">Remove</a>`}</td>`;
     return booksList.appendChild(trElement);
   });
 }
-
-function showUpdateBook(book){
-    const trElement = document.createElement("tr");
-    trElement.innerHTML = `<td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.pages}</td>
-    <td>${
-      book.status
-      ? `<button id='btn${book.id}' data-index=${book.id} class='btn btn-warning book-control-btn'>Read</button>`
-      : `<button id='btn${book.id}' data-index=${book.id} class='btn btn-primary book-control-btn'>Read Book</button>`
-    }</td>
-    <td>${`<a id='${book.id}' data-index=${book.id} href='#' class='btn btn-danger book-control-btn-detele' onclick="handleDeleteBook(this.id)">Remove</a>`}</td>`;
-    return booksList.appendChild(trElement);
-}
-
-function toggleBookStatus(btn) {
-  const bookId = btn.dataset.index;
-  console.log(`toggle ${bookId}`);
-}
-
-
