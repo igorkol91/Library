85e09f6fd5030ba2f8ac90f39dff5a5e0e30d00d
Igor.Koloski <koloski_igor@hotmail.com>
2021-07-01 10:48:12 +0200

    Add simple html

--- index.js
+++ index.js
@@ -6,15 +6,10 @@ createNew.addEventListener('click', () => {
   form.classList.toggle('d-none');
 });
 
-function(id) {
-
-}
-
 const books = [{ id: 1, title: 'name', author: 'author', status: false, pages: 100 }, { id: 2, title: 'name', author: 'author', status: true, pages: 100 }, { id: 3, title: 'name', author: 'author', status: true, pages: 100 }];
 
 books.map((book) => {
   const trElement = document.createElement('tr');
   trElement.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.status ? 'Read' : `<button id='btn${book.id}' data-index=${book.id} class='btn btn-primary'>ReadBook</button>`}</td>`;
-  
   return booksList.appendChild(trElement);
 });
